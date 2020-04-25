e21fe830297f2bfba4550ab18254ab803b1469f6
Snaikoo <SnakasArkan15@gmail.com>
2020-04-25 09:28:42 +0300

    BookShop + Filling

--- KVBook/Program.cs
+++ KVBook/Program.cs
@@ -4,11 +4,30 @@ namespace KVBook
 {
     class Book
     {
-        string Name;
-        string Author;
-        int Price;
+        public string Name;
+        public string Author;
+        public int Price;
     }
-
+    static class Librarium
+    {
+        static public Book[] BookShop = new Book[Size];
+        static public int Size;
+        static public void Booking()
+        {
+            Console.Write("Размер массива = ");
+            Size = Convert.ToInt32(Console.ReadLine());
+            int gnome = 0;
+            foreach (Book i in BookShop)
+            {                
+                Console.Write("Название книги №", gnome, " = ");
+                i.Name = Console.ReadLine();
+                Console.Write("Автор книги №", gnome, " = ");
+                i.Author = Console.ReadLine();
+                Console.Write("Цена книги №", gnome, " = ");
+                i.Price = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
